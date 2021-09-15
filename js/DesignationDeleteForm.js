16e869ff680e9d7fd59730cb8ec12344186b3ada
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-09-15 21:12:19 +0530

    Create DesignationDeleteForm.js

---
+++ js/DesignationDeleteForm.js
@@ -0,0 +1,17 @@
+function validate(frm)
+{
+var title=frm.title.value.trim();
+var titleErrorSection=document.getElementById('titleErrorSection');
+titleErrorSection.innerHTML='';
+if(title.length==0)
+{
+titleErrorSection.innerHTML='required';
+frm.title.focus();
+return false;
+}
+return true;
+}
+function cancelDeletion()
+{
+document.getElementById('cancelDeletionForm').submit();
+}
